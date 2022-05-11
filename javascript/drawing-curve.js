d6455c759eef5425a59f83954e22f8bd3e9aae36
Jeff Widjaja <jvwidjaja@gmail.com>
2022-05-12 00:26:39 +0800

    drawing_curve

--- javascript/drawing-curve.js
+++ javascript/drawing-curve.js
@@ -2,53 +2,101 @@ class DrawingCurve extends PaintFunction{
 
     constructor(contextReal,contextDraft){
         super();
-        this.contextReal = contextReal;
-        this.contextDraft = contextDraft;
-
-        this.origX = 0;
-        this.origY = 0;
-        this.endX = 0;
-        this.endY = 0;
-        this.controlX = 0;
-        this.controlY = 0;
-        this.mouseCounter = 0;
+        this.ctx = contextReal;
+        this.ctxDraft = contextDraft;
+        this.click = 0;
     }
 
-    onMouseDown(coord, event){
-        this.contextReal.strokeStyle = "#f11"
-        this.contextReal.lineWidth = 5
+    onMouseDown(coord, e){
+        
+        this.ctx.strokeStyle = '#f44'
+        this.ctxDraft.strokeStyle = '#f44'
 
+        this.ctx.lineWidth = 5;
+        this.ctxDraft.lineWidth = 5;
 
-
-        //start point
-        if(this.mouseCounter === 0){
-            this.contextReal.beginPath();
-            this.contextReal.moveTo(coord[0], coord[1]);
-
-            this.mouseCounter = 1
-            console.log(`start point: ${coord[0]}, ${coord[1]}`)
+        if(this.click === 0){
+            this.origX = coord[0]
+            this.origY = coord[1]
+            console.log(`${this.click} start point coordinate ${this.origX}, ${this.origY}`)
+            this.click++
+            
         }
 
-        //endpoint
-        else if(this.mouseCounter === 1){
+        else if(this.click === 1){
             this.endX = coord[0]
             this.endY = coord[1]
-
-            this.mouseCounter = 2
-            console.log(`end point: ${coord[0]}, ${coord[1]}`)
-        }
-
-        //control point 
-        else if(this.mouseCounter === 2){
-            this.controlX = coord[0]
-            this.controlY = coord[1]
+            console.log(`${this.click} end point coordinate ${this.endX}, ${this.endY}`)
+            this.click++
             
-            this.contextReal.quadraticCurveTo(this.endX, this.endY, this.controlX, this.controlY);
-            this.contextReal.stroke();
-            this.mouseCounter = 0
-            console.log(`control point: ${coord[0]}, ${coord[1]}`)
         }
 
+        else if(this.click === 2){
+            this.cpX = coord[0]
+            this.cpY = coord[1]
+            console.log(`${this.click} control point 1 coordinate ${this.cpX}, ${this.cpY}`)
+            //this.click = 0 
+            this.click++
+            
+        }
+
+        // else if(this.click === 3){
+        //     console.log("change to context real")
+        //     // Clearing the rectangle first
+
+        //     this.ctx.moveTo(this.origX, this.origY)
+        //     this.ctx.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+        //     this.ctx.stroke()
+        //     this.click = 0
+        // }
+ 
     }
+
+    onMouseMove(coord, e){
+        if(this.click == 1){
+            this.ctxDraft.beginPath();
+            this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+            
+            this.ctxDraft.moveTo(this.origX, this.origY);
+            this.ctxDraft.lineTo(coord[0], coord[1]);
+
+            this.ctxDraft.stroke();
+        } else if (this.click == 2){
+            this.ctxDraft.beginPath();
+            this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+
+            this.ctxDraft.moveTo(this.origX, this.origY);
+            this.ctxDraft.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+
+            this.ctxDraft.stroke();
+        }
+
+        else if(this.click == 3){
+            console.log("change to context real")
+            // Clearing the rectangle first
+
+            this.ctx.moveTo(this.origX, this.origY)
+            this.ctx.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+            this.ctx.stroke()
+            this.click = 0
+        }
+        
+    }   
+
+    // onMouseUp(coord) {
+    //     // Clearing the rectangle first
+    //     this.contextDraft.clearRect(
+    //         0,
+    //         0,
+    //         canvasDraft.width,
+    //         canvasDraft.height
+    //     );
+
+    //     this.ctx.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+    //     this.ctx.stroke()
+    // }
+
+    onMouseLeave() {}
+    onMouseEnter() {}
 
 }

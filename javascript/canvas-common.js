ed9650bbbe9b5e91af0cea34ac4fa28cfabe1937
GSAndrell <trafalgarandrell300@gmail.com>
2022-05-12 22:40:58 +0800

    reverted

--- javascript/canvas-common.js
+++ javascript/canvas-common.js
@@ -4,66 +4,58 @@
  ***********************************************/
 
 // changed codes to a way that we start with default color and choose color later.
+
 let canvasReal = document.getElementById("canvas-real");
 let contextReal = canvasReal.getContext("2d");
 let canvasDraft = document.getElementById("canvas-draft");
 let contextDraft = canvasDraft.getContext("2d");
-let context = canvasDraft.getContext("2d");
-let start_background_color = "white";
-context.fillStyle = start_background_color;
-context.fillRect(0, 0, canvasDraft.width, canvasDraft.height);
+let currentFunction;
+let dragging = false;
 
-let draw_color = "black";
-let draw_width = "2";
-let is_drawing = false;
+$("#canvas-draft").mousedown(function (e) {
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  currentFunction.onMouseDown([mouseX, mouseY], e);
+  dragging = true;
+});
 
+$("#canvas-draft").mousemove(function (e) {
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  if (dragging) {
+    currentFunction.onDragging([mouseX, mouseY], e);
+  }
+  currentFunction.onMouseMove([mouseX, mouseY], e);
+});
 
-function change_color(element) {
-    draw_color = element.style.background;
-}
+$("#canvas-draft").mouseup(function (e) {
+  dragging = false;
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  currentFunction.onMouseUp([mouseX, mouseY], e);
+});
 
+$("#canvas-draft").mouseleave(function (e) {
+  dragging = false;
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  currentFunction.onMouseLeave([mouseX, mouseY], e);
+});
 
+$("#canvas-draft").mouseenter(function (e) {
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  currentFunction.onMouseEnter([mouseX, mouseY], e);
+});
 
-canvasDraft.addEventListener("touchstart", start, false);
-canvasDraft.addEventListener("touchmove", draw, false);
-canvasDraft.addEventListener("mousedown", start, false);
-canvasDraft.addEventListener("mousemove", draw, false);
-
-canvasDraft.addEventListener("touchend", stop, false);
-canvasDraft.addEventListener("mouseup", stop, false);
-canvasDraft.addEventListener("mouseout", stop, false);
-
-function start(event) {
-    is_drawing = true;
-    context.beginPath();
-    context.moveTo(event.clientX - canvasDraft.offsetLeft,
-                    event.clientY - canvasDraft.offsetTop);
-    context.preventDefault();
-}
-
-function draw(event) {
-    if ( is_drawing ){
-        context.lineTo(event.clientX - canvasDraft.offsetLeft,
-                        event.clientY - canvasDraft.offsetTop);
-        context.strokeStyle = draw_color;
-        context.lineWidth = draw_width;
-        context.lineCap = "round";
-        context.lineJoin = "round";
-        context.stroke();
-    }
-    event.preventDefault();
-}
-
-function stop(event){
-    if ( is_drawing ){
-        context.stroke();
-        context.closePath();
-        is_drawing = false;
-    }
-    event.preventDefault();
-
-    if ( event.type != 'mouseout'){
-    restore_array.push(context.getImageData(0, 0, canvasDraft.width, canvasDraft.height));
-    index += 1;
-    }
+/** # Class (all classes will have these methods) #
+/*  ====================== */
+class PaintFunction {
+  constructor() {}
+  onMouseDown() {}
+  onDragging() {}
+  onMouseMove() {}
+  onMouseUp() {}
+  onMouseLeave() {}
+  onMouseEnter() {}
 }
